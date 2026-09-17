11ce446a534de0647ca4c31f7d65cd02948080a6
Tra-moudian <tramoudian1963@gmail.com>
2026-09-17 23:21:22 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE="prayerclock-v5-0-stable";
+const CACHE="prayerclock-v5-0-1-update-test";
 const META_CACHE="prayerclock-meta";
 const PUSH_SERVER="https://prayerclock-notifications.tramoudian1963.workers.dev";
 const APP_SHELL=[
